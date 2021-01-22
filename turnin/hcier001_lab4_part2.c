40e33eec432d2170fdcfc1cb7973a19c6036bda2
shrimpnoodles <77302524+shrimpnoodles@users.noreply.github.com>
2021-01-21 17:29:05 -0800

    Update hcier001_lab4_part2.c

--- turnin/hcier001_lab4_part2.c
+++ turnin/hcier001_lab4_part2.c
@@ -19,7 +19,10 @@ void Tick(){
 			state = init;
 			break;
 		case init:
-			if(PINA == 0X01){
+			if(PINA == 0x00){
+				state = init;
+			}
+			else if(PINA == 0x01){
 				state = p0push;
 			}
 			else if(PINA == 0x02){
@@ -28,44 +31,39 @@ void Tick(){
 			else if(PINA == 0x03){
 				state = reset;
 			}
-			else{
+			break;
+		case p0push:
+			if(PINA != 0x01){
 				state = init;
 			}
 			break;
-		case p0push:
-				if(PINA != 0x01){
-				state = init;
-				}
-			break;
 		case p1push:
-				if(PINA !=0x02){
+			if(PINA !=0x02){
 				state = init;
-				}
+			}
 			break;
 		case reset:
-				if(PINA!=0x03){
+			if(PINA != 0x03){
 				state = init;
-				}
+			}
 			break;
 		default:
 			state = start;
 			break;
 		}
 	switch(state){ //actions
-		case init:
-			break;
 		case p0push:
-			if(portcCount < 9){
+			if(PORTC < 9){
 				portcCount++;
 			}
 			break;
-		case p1push: 
-			if(portcCount > 0){
+		case p1push:
+			if(PORTC > 0){
 				portcCount--;
 			}
 			break;
 		case reset:
-			portcCount =0;
+			portcCount = 0;
 			break;
 		default:
 			break;
